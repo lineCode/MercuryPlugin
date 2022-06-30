97233422ed89e71ad3d2552dac92951f95e2d4c0
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2022-06-30 03:03:29 +0300

    Replace class name in PURE_VIRTUAL macros

--- Mercury/Source/MercuryHttp/Public/MercuryHttpClassBase.h
+++ Mercury/Source/MercuryHttp/Public/MercuryHttpClassBase.h
@@ -23,41 +23,41 @@ public:
 		Keywords = "Get URL Link Host Server"
 	))
 	virtual FString GetURL() const
-	PURE_VIRTUAL(UMercuryHttpBase::GetURL, return TEXT(""););
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetURL, return TEXT(""););
 
 	UFUNCTION(BlueprintPure, DisplayName = "Get URL Parameter", Category = "HTTP|Base", meta = (
 		Keywords = "Get URL Parameter Argument Link Server Name"
 	))
 	virtual FString GetURLParameter(const FString& ParameterName) const
-	PURE_VIRTUAL(UMercuryHttpBase::GetURLParameter, return TEXT(""););
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetURLParameter, return TEXT(""););
 
 	UFUNCTION(BlueprintPure, DisplayName = "Get Header", Category = "HTTP|Base", meta = (
 		Keywords = "Get Header Name"
 	))
 	virtual FString GetHeader(const FString& HeaderName) const
-	PURE_VIRTUAL(UMercuryHttpBase::GetHeader, return TEXT(""););
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetHeader, return TEXT(""););
 
 	UFUNCTION(BlueprintPure, DisplayName = "Get All Headers", Category = "HTTP|Base", meta = (
 		Keywords = "Get All Headers Names"
 	))
 	virtual TArray<FString> GetAllHeaders() const
-	PURE_VIRTUAL(UMercuryHttpBase::GetAllHeaders, return TArray<FString>(););
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetAllHeaders, return TArray<FString>(););
 
 	UFUNCTION(BlueprintPure, DisplayName = "Get Content Type", Category = "HTTP|Base", meta = (
 		Keywords = "Get Content Type"
 	))
 	virtual FString GetContentType() const
-	PURE_VIRTUAL(UMercuryHttpBase::GetContentType, return TEXT(""););
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetContentType, return TEXT(""););
 
 	UFUNCTION(BlueprintPure, DisplayName = "Get Content Length", Category = "HTTP|Base", meta = (
 		Keywords = "Get Content Length"
 	))
 	virtual int32 GetContentLength() const
-	PURE_VIRTUAL(UMercuryHttpBase::GetContentLength, return 0;);
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetContentLength, return 0;);
 
 	UFUNCTION(BlueprintPure, DisplayName = "Get Content", Category = "HTTP|Base", meta = (
 		Keywords = "Get Content Payload"
 	))
 	virtual TArray<uint8> GetContent() const
-	PURE_VIRTUAL(UMercuryHttpBase::GetContent, return TArray<uint8>(););
+	PURE_VIRTUAL(UMercuryHttpClassBase::GetContent, return TArray<uint8>(););
 };
