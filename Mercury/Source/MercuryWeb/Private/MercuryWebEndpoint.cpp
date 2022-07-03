52b9aa6dbcc0721b441419d34837e11337079277
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2022-07-04 00:36:40 +0300

    Implement Blueprint-friendly IPv4Endpoint

---
+++ Mercury/Source/MercuryWeb/Private/MercuryWebEndpoint.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright (c) 2022 Kaya Adrian
+
+#include "MercuryWebEndpoint.h"
+
+#include "Interfaces/IPv4/IPv4Endpoint.h"
+#include "MercuryWebAddress.h"
+
+
+UMercuryWebEndpoint::UMercuryWebEndpoint(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	Resource = MakeShareable(new FIPv4Endpoint());
+}
+
+UMercuryWebAddress* UMercuryWebEndpoint::GetAddress() const
+{
+	if (!Resource)
+		return nullptr;
+	
+	UMercuryWebAddress* const&& Address = NewObject<UMercuryWebAddress>();
+	*Address->GetResource() = Resource->Address;
+	return Address;
+}
+
+const UMercuryWebEndpoint* const& UMercuryWebEndpoint::GetAny()
+{
+	static const UMercuryWebEndpoint* const& WebEndpoint = NewObject<const UMercuryWebEndpoint>();
+	*WebEndpoint->GetResource() = FIPv4Endpoint::Any;
+	return WebEndpoint;
+}
+
+void UMercuryWebEndpoint::Initialize()
+{
+	FIPv4Endpoint::Initialize();
+}
+
+void UMercuryWebEndpoint::Parse(const FString& EndpointString, UMercuryWebEndpoint*& OutEndpoint)
+{
+	OutEndpoint = NewObject<UMercuryWebEndpoint>();
+	FIPv4Endpoint::Parse(EndpointString, *OutEndpoint->GetResource());
+}
+
+uint16 UMercuryWebEndpoint::GetPort() const
+{
+	return Resource ? Resource->Port : 0u;
+}
+
+FString UMercuryWebEndpoint::ToString() const
+{
+	return Resource ? Resource->ToString() : TEXT("");
+}
+
+FText UMercuryWebEndpoint::ToText() const
+{
+	return Resource ? Resource->ToText() : FText::GetEmpty();
+}
+
+TSharedPtr<FInternetAddr> UMercuryWebEndpoint::ToInternetAddr() const
+{
+	return Resource ? MakeShareable(&Resource->ToInternetAddr().Get()) : nullptr;
+}
+
+bool UMercuryWebEndpoint::FromHostAndPort(const FString& HostAndPortString, UMercuryWebEndpoint*& OutEndpoint)
+{
+	OutEndpoint = NewObject<UMercuryWebEndpoint>();
+	return FIPv4Endpoint::FromHostAndPort(HostAndPortString, *OutEndpoint->GetResource());
+}
+
+TSharedPtr<FInternetAddr> UMercuryWebEndpoint::ToInternetAddrIPv4() const
+{
+	return Resource ? MakeShareable(&Resource->ToInternetAddrIPV4().Get()) : nullptr;
+}
+
+int32 UMercuryWebEndpoint::K2_GetPort() const
+{
+	return GetPort();
+}
---
+++ Mercury/Source/MercuryWeb/Public/MercuryWebEndpoint.h
@@ -0,0 +1,74 @@
+﻿// Copyright (c) 2022 Kaya Adrian
+
+#pragma once
+
+#include "MercuryCommon/Public/ResourceOwner.h"
+
+#include "MercuryWebEndpoint.generated.h"
+
+struct FIPv4Endpoint;
+
+class UMercuryWebAddress;
+
+
+UCLASS(Blueprintable, BlueprintType, DisplayName = "Mercury Web Address")
+class MERCURYWEB_API UMercuryWebEndpoint : public UObject, public TResourceOwner<FIPv4Endpoint>
+{
+	GENERATED_BODY()
+
+public:
+	explicit UMercuryWebEndpoint(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintPure, DisplayName = "Get Address", Category = "Web|IPv4Endpoint", meta = (
+		CompactNodeTitle = "Address",
+		Keywords = "Web IPv4Endpoint Get Address"
+	))
+	virtual UMercuryWebAddress* GetAddress() const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "Get Any", Category = "Web|IPv4Endpoint", meta = (
+		CompactNodeTitle = "Any",
+		Keywords = "Static Web IPv4Endpoint Get Any"
+	))
+	static const UMercuryWebEndpoint* const& GetAny();
+
+	UFUNCTION(BlueprintCallable, DisplayName = "Initialize", Category = "Web|IPv4Endpoint", meta = (
+		Keywords = "Static Web IPv4Endpoint Initialize"
+	))
+	static void Initialize();
+
+	UFUNCTION(BlueprintPure, DisplayName = "Parse", Category = "Web|IPv4Endpoint", meta = (
+		Keywords = "Static Web IPv4Endpoint Parse Endpoint String"
+	))
+	static void Parse(const FString& EndpointString, UMercuryWebEndpoint*& OutEndpoint);
+
+	virtual uint16 GetPort() const;
+	
+	UFUNCTION(BlueprintPure, DisplayName = "To String", Category = "Web|IPv4Endpoint", meta = (
+		CompactNodeTitle = "Cast",
+		Keywords = "Web IPv4Endpoint Cast To String"
+	))
+	virtual FString ToString() const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "To Text", Category = "Web|IPv4Endpoint", meta = (
+		CompactNodeTitle = "Cast",
+		Keywords = "Web IPv4Endpoint Cast To Text"
+	))
+	virtual FText ToText() const;
+
+	virtual TSharedPtr<FInternetAddr> ToInternetAddr() const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "From Host And Port", Category = "Web|IPv4Endpoint", meta = (
+		Keywords = "Static Web IPv4Endpoint From Host And Port String Endpoint"
+	))
+	static UPARAM(DisplayName = "Success") bool
+	FromHostAndPort(const FString& HostAndPortString, UMercuryWebEndpoint*& OutEndpoint);
+
+	virtual TSharedPtr<FInternetAddr> ToInternetAddrIPv4() const;
+
+protected:
+	UFUNCTION(BlueprintPure, DisplayName = "Get Port", Category = "Web|IPv4Endpoint", meta = (
+		CompactNodeTitle = "Port",
+		Keywords = "Web IPv4Endpoint Get Port"
+	))
+	virtual int32 K2_GetPort() const;
+};
