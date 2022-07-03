ed257c84c0e6f21ceb680fd5d21f3e613359a65a
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2022-07-03 23:50:43 +0300

    Implement Blueprint-friendly FIPv4Subnet

---
+++ Mercury/Source/MercuryWeb/Private/MercuryWebSubnet.cpp
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2022 Kaya Adrian
+
+#include "MercuryWebSubnet.h"
+
+#include "MercuryWebAddress.h"
+#include "Interfaces/IPv4/IPv4Subnet.h"
+
+
+UMercuryWebSubnet::UMercuryWebSubnet(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	Resource = MakeShareable(new FIPv4Subnet());
+}
+
+UMercuryWebAddress* UMercuryWebSubnet::GetAddress() const
+{
+	if (!Resource)
+		return nullptr;
+	
+	UMercuryWebAddress* const&& Address = NewObject<UMercuryWebAddress>();
+	*Address->GetResource() = Resource->Address;
+	return Address;
+}
+/*
+UMercuryWebSubnetMask* UMercuryWebSubnet::GetMask() const
+{
+	if (!Resource)
+		return nullptr;
+
+	UMercuryWebSubnetMask* const&& Mask = NewObject<UMercuryWebSubnetMask>();
+	*Mask->GetResource() = Resource->Mask;
+	return Mask;
+}*/
+
+UMercuryWebAddress* UMercuryWebSubnet::BroadcastAddress() const
+{
+	if (!Resource)
+		return nullptr;
+
+	UMercuryWebAddress* const&& Address = NewObject<UMercuryWebAddress>();
+	*Address->GetResource() = Resource->BroadcastAddress();
+	return Address;
+}
+
+bool UMercuryWebSubnet::ContainsAddress(const UMercuryWebAddress* const& TestAddress) const
+{
+	return Resource && Resource->ContainsAddress(*TestAddress->GetResource());
+}
+
+FString UMercuryWebSubnet::ToString() const
+{
+	return Resource ? Resource->ToString() : TEXT("");
+}
+
+FText UMercuryWebSubnet::ToText() const
+{
+	return Resource ? Resource->ToText() : FText::GetEmpty();
+}
+
+void UMercuryWebSubnet::Parse(const FString& SubnetString, UMercuryWebSubnet*& OutSubnet)
+{
+	OutSubnet = NewObject<UMercuryWebSubnet>();
+	FIPv4Subnet::Parse(SubnetString, *OutSubnet->GetResource());
+}
---
+++ Mercury/Source/MercuryWeb/Public/MercuryWebSubnet.h
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2022 Kaya Adrian
+
+#pragma once
+
+#include "MercuryCommon/Public/ResourceOwner.h"
+
+#include "MercuryWebSubnet.generated.h"
+
+struct FIPv4Subnet;
+struct FIPv4SubnetMask;
+
+class UMercuryWebAddress;
+//class UMercuryWebSubnetMask;
+
+
+UCLASS(Blueprintable, BlueprintType, DisplayName = "Mercury Web Subnet")
+class MERCURYWEB_API UMercuryWebSubnet : public UObject, public TResourceOwner<FIPv4Subnet>
+{
+	GENERATED_BODY()
+
+public:
+	explicit UMercuryWebSubnet(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintPure, DisplayName = "Get Address", Category = "Web|IPv4Subnet", meta = (
+		CompactNodeTitle = "Address",
+		Keywords = "Web IPv4Subnet Get Address"
+	))
+	virtual UMercuryWebAddress* GetAddress() const;
+/*
+	UFUNCTION(BlueprintPure, DisplayName = "Get Mask", Category = "Web|IPv4Subnet", meta = (
+		CompactNodeTitle = "Mask",
+		Keywords = "Web IPv4Subnet Get Mask"
+	))
+	virtual UMercuryWebSubnetMask* GetMask() const;*/
+
+	UFUNCTION(BlueprintPure, DisplayName = "Broadcast Address", Category = "Web|IPv4Subnet", meta = (
+		Keywords = "Web IPv4Subnet Broadcast Address"
+	))
+	virtual UMercuryWebAddress* BroadcastAddress() const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "Contains Address", Category = "Web|IPv4Subnet", meta = (
+		Keywords = "Web IPv4Subnet Contains Address TestAddress"
+	))
+	virtual bool ContainsAddress(const UMercuryWebAddress* const& TestAddress) const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "To String", Category = "Web|IPv4Subnet", meta = (
+		CompactNodeTitle = "Cast",
+		Keywords = "Web IPv4Subnet Cast To String"
+	))
+	virtual FString ToString() const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "To Text", Category = "Web|IPv4Subnet", meta = (
+		CompactNodeTitle = "Cast",
+		Keywords = "Web IPv4Subnet Cast To Text"
+	))
+	virtual FText ToText() const;
+
+	UFUNCTION(BlueprintPure, DisplayName = "Parse", Category = "Web|IPv4Subnet", meta = (
+		Keywords = "Static Web IPv4Subnet Parse Subnet String"
+	))
+	static void Parse(const FString& SubnetString, UMercuryWebSubnet*& OutSubnet);
+};
