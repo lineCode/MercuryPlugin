02cd0ae774f0d7ba4eb3246fda6e8d6382963dcd
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2022-07-11 01:59:18 +0300

    Fix Bytes <-> String cast functions

---
+++ Mercury/Source/MercuryUtilities/Private/MercuryUtilitiesLibrary.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2022 Kaya Adrian
+
+#include "MercuryUtilitiesLibrary.h"
+
+
+FString UMercuryUtilitiesLibrary::ByteArrayToString(const TArray<uint8>& In, const int32 Count)
+{
+	const FString&& BrokenString = BytesToString(In.GetData(), Count);
+	FString&& FixedString = TEXT("");
+
+	for (int32&& Index = 0; Index < BrokenString.Len(); ++Index)
+	{
+		FixedString.AppendChar(BrokenString[Index] - 1);
+	}
+	return FixedString;
+}
+
+int32 UMercuryUtilitiesLibrary::StringToByteArray(
+	const FString& String,
+	TArray<uint8>& OutBytes,
+	const int32 MaxBufferSize
+)
+{
+	uint8*&& BrokenBytes = new uint8[MaxBufferSize];
+	OutBytes.SetNumUninitialized(MaxBufferSize);
+	
+	const int32&& BytesCopied = StringToBytes(String, BrokenBytes, MaxBufferSize);
+	for (int32&& Index = 0; Index < BytesCopied; ++Index)
+	{
+		OutBytes[Index] = BrokenBytes[Index] + 1;
+	}
+	OutBytes.Shrink();
+
+	delete[] BrokenBytes;
+	return BytesCopied;
+}
---
+++ Mercury/Source/MercuryUtilities/Public/MercuryUtilitiesLibrary.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2022 Kaya Adrian
+
+#pragma once
+
+#include "MercuryUtilitiesLibrary.generated.h"
+
+
+UCLASS(Abstract, DisplayName = "Mercury Utilities Blueprint Function Library")
+class MERCURYUTILITIES_API UMercuryUtilitiesLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, DisplayName = "Bytes To String", Category = "Mercury|Utilities", meta = (
+		CompactNodeTitle = "Cast",
+		Keywords = "In Byte Bytes Array Count Cast To String"
+	))
+	static FString ByteArrayToString(const TArray<uint8>& In, int32 Count);
+
+	UFUNCTION(BlueprintPure, DisplayName = "String To Bytes", Category = "Mercury|Utilities", meta = (
+		CompactNodeTitle = "Cast",
+		Keywords = "String Cast To Out Byte Bytes Array Max Buffer Size"
+	))
+	static UPARAM(DisplayName = "Bytes Copied") int32
+	StringToByteArray(const FString& String, TArray<uint8>& OutBytes, int32 MaxBufferSize);
+};
